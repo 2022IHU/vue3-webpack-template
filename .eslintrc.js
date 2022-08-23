2c882fda9af85771739218959fe2b7c9e77fc87a
IHU <2022ihu@gmail.com>
2022-08-23 12:40:23 +0900

    vue3 test

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: TRUE
+  },
+  extends: [
+    // vue
+    // 'plugin:vue/veu3-essential', // LV1
+    'plugin:vue/veu3-strongly-recommended', // LV2
+    // 'plugin:vue/veu3-recommended', // LV3
+    // js
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  rules: {
+    
+  }
+}
